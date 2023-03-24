f3e2cc7e7d18311a4cc86d1e3d23c43d386fe957
Francsico Felissia <francsicofelissia@gmail.com>
2023-03-24 18:50:26 -0300

    Se mejoró el ejemplo de implementación anterior

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,30 @@
   Se distribulle bajo la licencia GPLv.3 y se puede obtener desde
   https://github.com/franfelissia/Robot junto con toda la documentación necesaria.
 */
-
+#include <Arduino.h>
 #include "L298.h"
 #include "HC-SR04.h"
 #include "PinChangeInterrupt.h"
 
-// Definimos componentes extras
+// Definimos componentes extras.
 #define BTN 9
 #define L1 12
 #define L2 13
 
-// Definimos algunos datos de la interrupción
-#define threshold 400
+// Definimos algunos datos de la interrupción.
+#define threshold 200
 long time = 0;
 bool on = 0;
 
-// Inicializamos los módulos
+// Inicializamos los módulos.
 HCSR h04 = HCSR(2,3);
 L298N motors = L298N(4, 5, 6, 7, 10, 11);
 
 // Servicio de interrupción
 void interrupt(){
   /*
-    Si el tiempo transcurido entre la ultima llamada
-    y la llamada actual es mayor al umbral entonces
-    se cambia el valor de la variable on
+    Si el tiempo transcurido entre la ultima llamada y la llamada actual es mayor al umbral
+    entonces se cambia el valor de la variable on.
   */
   if(millis() - time > threshold)
     on = !on;
@@ -47,28 +46,49 @@ void setup(){
   pinMode(L1, OUTPUT);
   pinMode(L2, OUTPUT);
   digitalWrite(L1, 0);
-  digitalWrite(L2, 0);
-    
+  digitalWrite(L2, 1);
   attachPCINT(digitalPinToPCINT(BTN), interrupt, RISING);
+
+  motors.power(127);
+  motors.forward();
 }
 
-void loop(){
+void loop(){ 
+  // Control de encendido y apagado (Se puede mejorar con el modo bajo consumo)
   if(on == 1){
+    motors.power(127);
+    motors.forward();
+    digitalWrite(L1, 0);
+    digitalWrite(L2, 1);
+
     uint8_t dist = h04.read();
     
-    if(dist < 30){
-      motors.back();
-      motors.power(127);
+    // Si se encuentra con algo.
+    if(dist < 10){
+      
+      //Cambia las luces.
       digitalWrite(L1, 1);
       digitalWrite(L2, 0);
-    }
-    else{
-      motors.power(0);      
-      digitalWrite(L1, 0);
-      digitalWrite(L2, 1);
+      
+      //Para.
+      motors.power(0);
+      
+      //Espera.
+      delay(500);
+      
+      //Se hace para atras.
+      motors.back();
+      motors.power(127);
+      delay(500);
+      
+      //Elige si va para donde ir al azar.
+      if(random(2)) motors.left();
+      else motors.right();
+      delay(random(250, 500));
     }
   }
   else{
+    motors.power(0);
     digitalWrite(L1, 0);
     digitalWrite(L2, 0);
   }
